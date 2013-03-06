7339e639f399c274149a2db19e305800f6bf4e7a
readparse <john@funnycow.com>
2013-03-06 02:09:56 -0600

    stopping point

--- load_items.asmx.cs
+++ load_items.asmx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Services;
 using System.Web.Script.Services;
 using System.Xml.Linq;
+using System.Collections.ObjectModel;
 namespace ItemLoader
 {
     /// <summary>
@@ -18,6 +19,7 @@ namespace ItemLoader
     [System.Web.Script.Services.ScriptService]
     public class load_items1 : System.Web.Services.WebService
     {
+        Sitecore.Data.Database master = Sitecore.Data.Database.GetDatabase("master");
         [WebMethod]
         public List<string> upload()
         {
@@ -57,8 +59,19 @@ namespace ItemLoader
                                         {
                                             if (field.Name == "Field")
                                             {
+
                                                 string name = field.Attribute("Name").Value;
                                                 string value = field.Attribute("Value").Value;
+                                                
+                                                
+                                                if (field.Attributes().Any(p => p.Name == "Type")) {
+                                                    string type = field.Attribute("Type").Value;
+                                                    if (type == "Date")
+                                                    {
+                                                        value = Sitecore.DateUtil.ToIsoDate(DateTime.Parse(value));
+                                                    }
+                                                }
+
                                                 newItem.Fields[name].Value = value;
                                             }
                                         }
@@ -76,6 +89,70 @@ namespace ItemLoader
                 
             }
             return item_id_list;
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public Collection<Item> get_items()
+        {
+            Collection<Item> items = new Collection<Item>();
+            HttpRequest req = HttpContext.Current.Request;
+            string list = req.Params["list"];
+            string[] id_list = list.Split(':');
+            foreach (string id in id_list)
+            {
+                var i = new Item();
+                i.id = id;
+                i.load();
+                items.Add(i);
+            }
+            return items;
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public Item get_item()
+        {
+            HttpRequest req = HttpContext.Current.Request;
+            string id = req.Params["id"];
+            Item item = new Item();
+            item.id = id;
+            item.load();
+            return item;
+        }
+
+        [WebMethod]
+        public int spiderman()
+        {
+            HttpRequest req = HttpContext.Current.Request;
+            string list = req.Params["list"];
+            string[] id_list = list.Split(':');
+            //return id_list.Count();
+            int count = 0;
+            using (new Sitecore.SecurityModel.SecurityDisabler())
+            {
+                foreach (string id in id_list)
+                {
+                    var item = master.GetItem(new Sitecore.Data.ID(id));
+                    item.Delete();
+                    count++;
+                }
+            }
+            return count;
+            
+        }
+
+        [WebMethod]
+        public bool delete_item()
+        {
+            HttpRequest req = HttpContext.Current.Request;
+            string id = req.Params["id"];
+            using (new Sitecore.SecurityModel.SecurityDisabler())
+            {
+                var item = master.GetItem(new Sitecore.Data.ID(id));
+                item.Delete();
+            }
+            return true;
         }
 
         [WebMethod]
@@ -98,14 +175,82 @@ namespace ItemLoader
                         if (tag.Name == "{/}string")
                         {   
                             Sitecore.Data.ID itemID = Sitecore.Data.ID.Parse(tag.Value);
-                            Sitecore.Data.Items.Item item = master.GetItem(itemID);
-                            item.Delete();
-                            item_id_list.Add(item.ID.ToString());
+                            try
+                            {
+                                Sitecore.Data.Items.Item item = master.GetItem(itemID);
+                                item.Delete();
+                                item_id_list.Add(item.ID.ToString());
+                            }
+                            catch(Exception e)
+                            {
+                                //swallow
+                            }
                         }
                     }
                 }
             }
             return item_id_list;
         }
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public bool publish()
+        {
+            Sitecore.Data.Database master = Sitecore.Data.Database.GetDatabase("master");
+            Sitecore.Data.Database web = Sitecore.Data.Database.GetDatabase("web");
+            var options = new Sitecore.Publishing.PublishOptions(master, web, Sitecore.Publishing.PublishMode.Smart, Sitecore.Globalization.Language.Parse("en"), System.DateTime.Now);
+            var publisher = new Sitecore.Publishing.Publisher(options);
+
+            publisher.Options.RootItem = master.GetItem("/sitecore/content/home");
+            publisher.Options.Deep = true;
+            publisher.Publish();
+            return true;
+        }
+    }
+    public class Item
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public Collection<Field> fields = new Collection<Field>();
+
+        public void load()
+        {
+            Sitecore.Data.Database master = Sitecore.Data.Database.GetDatabase("master");
+            Sitecore.Data.Items.Item item = master.GetItem( new Sitecore.Data.ID(this.id) );
+            this.name = item.Name;
+            foreach (Sitecore.Data.Fields.Field field in item.Fields)
+            {
+                Field ff = new Field(field.Name, field.Value, field.Type);
+                this.fields.Add(ff);
+            }
+        }
+    }
+    
+    
+    public class Field
+    {
+        public string name { get; set; }
+        public string value { get; set; }
+        public string type { get; set; }
+
+        public Field(string name, string value, string type)
+        {
+            this.name = name;
+            this.value = value;
+            this.type = type;
+        }
+        public Field(string name, string value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+        public Field(string name)
+        {
+            this.name = name;
+        }
+        public Field()
+        {
+
+        }
+
     }
 }
